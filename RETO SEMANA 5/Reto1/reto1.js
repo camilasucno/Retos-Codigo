6690f7c5cec1401858b0f25c2f06f270d0303678
Camila Sucno <camilasucno22@gmail.com>
2022-04-08 18:02:16 -0500

    update

---
+++ RETO SEMANA 5/Reto1/reto1.js
@@ -0,0 +1,9 @@
+let PG = prompt("Ingrese el precio del galon");
+
+let L = number(prompt("Ingrese los litros de leche producidos"))
+
+let TG = L/3.785
+
+let GA = TG*PG
+
+console.log(`La ganancia por la entrega de leche es de: ${ga} soles`)
